9ab87a4cb29692074e5b8c1134ebf880c46c8e3c
jjeongminchoi <chivalry1107@gmail.com>
2023-12-16 23:24:26 +0900

    project commit

---
+++ src/main/java/com/jm/hinamaste/HiNamasteApplication.java
@@ -0,0 +1,13 @@
+package com.jm.hinamaste;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiNamasteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HiNamasteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/jm/hinamaste/HiNamasteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jm.hinamaste;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HiNamasteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

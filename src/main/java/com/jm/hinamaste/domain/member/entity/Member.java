c8bb2a3d48a5a2665d386ad596d64f1dae27d5d2
jjeongminchoi <chivalry1107@gmail.com>
2024-01-14 19:51:00 +0900

    BaseEntity 추가

--- src/main/java/com/jm/hinamaste/domain/member/entity/Member.java
+++ src/main/java/com/jm/hinamaste/domain/member/entity/Member.java
@@ -4,6 +4,7 @@ import com.jm.hinamaste.domain.member.constant.MemberStatus;
 import com.jm.hinamaste.domain.member.constant.MemberType;
 import com.jm.hinamaste.domain.member.constant.SexType;
 import com.jm.hinamaste.domain.member.dto.MemberEdit;
+import com.jm.hinamaste.global.audit.BaseEntity;
 import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
@@ -19,9 +20,10 @@ import static lombok.AccessLevel.*;
 @Getter
 @NoArgsConstructor(access = PROTECTED)
 @Entity
-public class Member implements Serializable { // 사용자 인증 정보를 세션에 저장하고 복원하기 위해 내부적으로 객체 직렬화를 사용(spring-session-jdbc)
+public class Member extends BaseEntity implements Serializable { // 사용자 인증 정보를 세션에 저장하고 복원하기 위해 내부적으로 객체 직렬화를 사용(spring-session-jdbc)
 
     @Id @GeneratedValue(strategy = IDENTITY)
+    @Column(name = "member_id")
     private Long id;
 
     private String email;
---
+++ src/main/java/com/jm/hinamaste/global/audit/BaseEntity.java
@@ -0,0 +1,23 @@
+package com.jm.hinamaste.global.audit;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Getter
+@EntityListeners(AuditingEntityListener.class)
+@MappedSuperclass
+public abstract class BaseEntity extends BaseTimeEntity {
+
+    @CreatedBy
+    @Column(nullable = false, updatable = false)
+    private String createdBy;
+
+    @LastModifiedBy
+    @Column(nullable = false)
+    private String updatedBy;
+}

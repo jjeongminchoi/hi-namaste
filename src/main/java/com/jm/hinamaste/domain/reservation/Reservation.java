10b7975073b8d79383f9f2fc7e7c7599eb446580
jjeongminchoi <chivalry1107@gmail.com>
2024-02-05 23:25:45 +0900

    Reservation 추가

---
+++ src/main/java/com/jm/hinamaste/domain/reservation/Reservation.java
@@ -0,0 +1,25 @@
+package com.jm.hinamaste.domain.reservation;
+
+import com.jm.hinamaste.domain.course.entity.Course;
+import com.jm.hinamaste.domain.member.entity.Member;
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+public class Reservation {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "reservation_id")
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Member member;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Course course;
+}
